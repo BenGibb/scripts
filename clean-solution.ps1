ae10eda490c03fd6d2762119d6f07120664bb743
zblesk <zblesk@zblesk.net>
2013-09-18 13:01:18 +0200

    Initial commit

---
+++ clean-solution.ps1
@@ -0,0 +1,51 @@
+<#
+.SYNOPSIS
+    Cleans up a folder and its subfolders, removing files you generally don't want to send along with a solution.  (Namely, the BIN and OBJ folders and stray PDB files.)
+
+.PARAMETER whatif 
+    Only runs the deletion in whatif mode, NOT deleting any files.
+
+.PARAMETER delete 
+    Deletes the files. This parameter is required, in order to avoid random file deletion.
+    Only works if -whatif was not specified. 
+
+.PARAMETER tfs 
+    Also deletes TFS binding files.
+#>  
+
+param([string]$path,
+    [switch]$whatif,
+    [switch]$delete,
+    [switch]$tfs)
+
+if (-not ($whatif) `
+    -and -not ($delete) `
+    -and -not ($tfs))
+{
+    echo @"
+    Specify a parameter: -whatif, -delete, -tfs
+    For more info, run Get-Help clean-solution.ps1 -Detailed
+
+"@
+    exit
+}
+
+$oldPath = Get-Location
+cd $path
+
+$params = @{
+    whatif = $whatif
+    recurse = $true
+}
+
+# Clean a solution from unnecessary stuff, i.e. before sending it via mail.
+ls bin -r | ri @params #remove BIN folders
+ls obj -r | ri @params #remove OBJ folders
+ls *.pdb -r | ri @params #remove remaining PDBs, if any
+if ($tfs) #remove TFS mappings
+{
+    ls *.vssscc -r | ri @params
+    ls *.vspscc -r | ri @params
+}
+
+cd $oldPath
