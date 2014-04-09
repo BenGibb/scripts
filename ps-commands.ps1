4b524c3b5fb747d6247d62c8f2db7d4343bc539b
zblesk <zblesk@zblesk.net>
2014-04-09 14:37:57 +0200

    New: kill-webservers() kills all the currently running instances of Visual Studio's web dev servers.
    Improved: start-server() now doesn't fail if Python 2.x isn't installed.

--- ps-commands.ps1
+++ ps-commands.ps1
@@ -1,9 +1,19 @@
 function start-server()
 {
-  python -m http.server
+  $out =  & python -m http.server 2>&1
+  if ($out -match "No module named http")
+  {
+    echo 'Python 2.x not found. Trying Python 3.x'
+    python -m SimpleHTTPServer
+  }
 }
 
 function chocolatey-versions()
 {
   cver all -localonly
+}
+
+function kill-webservers()
+{
+    kill -processname WebDev.WebServer40
 }
