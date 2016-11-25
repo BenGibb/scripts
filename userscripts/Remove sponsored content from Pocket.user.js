76214d1ba38061515223850784b172597ee6b82c
Laci <bladislav@gmail.com>
2016-11-25 21:50:18 +0100

    Adding @run-at document-idle setting to Pocket userscript

--- userscripts/Remove sponsored content from Pocket.user.js
+++ userscripts/Remove sponsored content from Pocket.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Remove sponsored content from Pocket
 // @namespace    https://zblesk.net/blog
-// @version      0.91
+// @version      0.92
 // @description  Surprisingly, it removes sponsored content from Pocket.
 // @author       zblesk
 // @match        https://getpocket.com/a/queue/
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
