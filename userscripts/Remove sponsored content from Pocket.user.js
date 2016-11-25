0c48fb2c25db60d207e29c90f97ab327ea9bafac
zblesk <bladislav@gmail.com>
2016-11-25 19:47:16 +0100

    Adding Pocket userscript

---
+++ userscripts/Remove sponsored content from Pocket.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Remove sponsored content from Pocket
+// @namespace    https://zblesk.net/blog
+// @version      0.9
+// @description  Surprisingly, it removes sponsored content from Pocket.
+// @author       zblesk
+// @match        https://getpocket.com/a/queue/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('Removing all ' + $(".spoc").length + ' items with the "spoc" class.');
+    $(".spoc").hide();
+})();
