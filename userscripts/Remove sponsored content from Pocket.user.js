4d7b355ec5742c712d4dbe4e66db909beb1e3dd4
zblesk <bladislav@gmail.com>
2016-11-30 14:52:33 +0100

    Adding a delay to Pocket userscript

--- userscripts/Remove sponsored content from Pocket.user.js
+++ userscripts/Remove sponsored content from Pocket.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Remove sponsored content from Pocket
 // @namespace    https://zblesk.net/blog
-// @version      0.92
-// @description  Surprisingly, it removes sponsored content from Pocket.
+// @version      0.93
+// @description  It tries to remove sponsored content from Pocket's queue.
 // @author       zblesk
 // @match        https://getpocket.com/a/queue/
 // @grant        none
@@ -11,6 +11,8 @@
 
 (function() {
     'use strict';
-    console.log('Removing all ' + $(".spoc").length + ' items with the "spoc" class.');
-    $(".spoc").hide();
+    setTimeout(function() {
+      console.log('Removing all ' + $(".spoc").length + ' items with the "spoc" class.');
+      $(".spoc").hide();
+    }, 5000);
 })();
