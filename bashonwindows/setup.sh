02f1b47b209ad6a3619805179b960345a2082728
zblesk <bladislav@gmail.com>
2017-04-21 12:32:21 +0200

    .

--- bashonwindows/setup.sh
+++ bashonwindows/setup.sh
@@ -5,10 +5,12 @@ pushd .vim/colors
 curl -O https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim
 popd -2
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 cp .vimrc ~
 cp .tmux.conf ~
 cp .bashrc ~
 
 apt-get install mc
 
-curl -O ~/.dircolors https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark 
+curl https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark > ~/.dircolors 
