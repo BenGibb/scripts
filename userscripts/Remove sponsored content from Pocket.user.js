0c2c240209f7c84db2c61f7399c025a1905f2812
zblesk <bladislav@gmail.com>
2017-10-24 10:19:33 +0200

    Adding a new matcher for Pocket URL.

--- userscripts/Remove sponsored content from Pocket.user.js
+++ userscripts/Remove sponsored content from Pocket.user.js
@@ -4,15 +4,16 @@
 // @version      0.93
 // @description  It tries to remove sponsored content from Pocket's queue.
 // @author       zblesk
-// @match        https://getpocket.com/a/queue*
+// @match        https://getpocket.com/a/queue/
+// @match        https://getpocket.com/a/queue/list/
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    setTimeout(function() {
-      console.log('Removing all ' + $(".spoc").length + ' items with the "spoc" class.');
-      $(".spoc").hide();
-    }, 1000);
+  'use strict';
+  setTimeout(function() {
+    console.log('Removing all ' + $(".spoc").length + ' items with the "spoc" class.');
+    $(".spoc").hide();
+  }, 1000);
 })();
